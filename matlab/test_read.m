12cd6c6e63c30acf7539a22426648a8fe0b5ef6a
Takuya Miyashita <miyashita@hydrocoast.jp>
2020-01-30 17:51:05 +0900

    add printing in matlab

--- matlab/test_read.m
+++ matlab/test_read.m
@@ -8,7 +8,7 @@ filename = 'depth_2430-01.dat';
 % parameters
 nx = 720;
 ny = 540;
-
+% formatter
 arraydata = formatter(fullfile(datapath,filename),nx,ny);
 
 
@@ -18,8 +18,19 @@ dy = dx;
 x = -1.2000e6:dx:0.5496e6-dx;
 y = -1.2060e6:dy:0.1062e6-dy;
 [X,Y] = meshgrid(x,y);
-%
+
+% plot
 pcolor(X,Y,-arraydata); shading interp;
 axis equal tight
 demcmap([-7000 3000])
-% cb = colorbar;
+cb = colorbar;
+
+
+% % % output
+% fileout = 'matrix_depth_2430-01.csv';
+% fmt = ['%11.3E',repmat(',%11.3E',[1, nx-1]),'\n'];
+% % % print
+% fid = fopen(fileout,'w');
+% fprintf(fid,fmt,-arraydata');
+% fclose(fid);
+% 
