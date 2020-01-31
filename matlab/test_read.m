e74c2b8e83b3f43d5012efb2c8fa37e80f1ceb84
Takuya Miyashita <miyashita@hydrocoast.jp>
2020-01-31 14:24:49 +0900

    update format

--- matlab/test_read.m
+++ matlab/test_read.m
@@ -27,10 +27,10 @@ cb = colorbar;
 
 
 % % % output
-% fileout = 'matrix_depth_2430-01.csv';
-% fmt = ['%11.3e',repmat(',%11.3e',[1, nx-1]),'\n'];
-% % % print
-% fid = fopen(fileout,'w');
-% fprintf(fid,fmt,-arraydata');
-% fclose(fid);
+fileout = 'matrix_depth_2430-01.csv';
+fmt = ['%14.6e',repmat(',%14.6e',[1, nx-1]),'\n'];
+% % print
+fid = fopen(fileout,'w');
+fprintf(fid,fmt,-arraydata');
+fclose(fid);
 
